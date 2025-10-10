dc6952e5765218ca996dc168d39566e1d6510cab
Stepan <stepan@MacBook-Pro-Stepan.local>
2025-10-09 23:19:16 -0700

    Добавил комментарии в файл circle,py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    ''' Принимается число r, возвращает pi*r^2(площадь круга) '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    ''' Принимается число r, возвращает 2*pi*r(периметр круга) '''
     return 2 * math.pi * r
 
